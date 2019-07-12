5b38fcdf27ac98ddc91508d8ee818bedd411d1f8
Henry Stroud <henry.stroud@hotmail.co.uk>
2019-07-12 13:15:20 +0100

    added background

--- rn/App.js
+++ rn/App.js
@@ -1,19 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>TEST</Text>
-    </View>
-  );
+import React from 'react'
+import Main from './app/Main'
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ rn/app/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import { LinearGradient } from 'expo'
+import { primaryGradientArray } from './utils/Colors'
+export default class Main extends React.Component {
+  render() {
+    return (
+      <LinearGradient colors={primaryGradientArray} style={styles.container}>
+        <Text> Open up App.js to start working on your app! </Text>
+      </LinearGradient>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
