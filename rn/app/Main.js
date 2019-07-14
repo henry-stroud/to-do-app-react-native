3281c8b99c74811ef1e9d22ec2bcc3a3b40f670e
Henry Stroud <henry.stroud@hotmail.co.uk>
2019-07-14 16:38:17 +0100

    changed to do title

--- rn/app/Main.js
+++ rn/app/Main.js
@@ -2,17 +2,26 @@ import React from 'react'
 import { StyleSheet, Text, View, StatusBar } from 'react-native'
 import { LinearGradient } from 'expo'
 import { primaryGradientArray } from './utils/Colors'
+import Header from './components/Header';
+const headerTitle = 'To Do List';
+
 export default class Main extends React.Component {
   render() {
     return (
       <LinearGradient colors={primaryGradientArray} style={styles.container}>
-        <Text> Open up App.js to start working on your app! </Text>
+        <View style={styles.centered}>
+          <Header title={headerTitle} />
+        </View>
       </LinearGradient>
     )
   }
 }
 const styles = StyleSheet.create({
+  centered: {
+    alignItems: 'center'
+  },
   container: {
     flex: 1
   }
+
 })
