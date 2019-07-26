0b98975393f4cdb51bae92e07749df314b67ea9d
Henry Stroud <henry.stroud@hotmail.co.uk>
2019-07-26 14:18:38 +0100

    added input field

--- rn/app/Main.js
+++ rn/app/Main.js
@@ -2,15 +2,29 @@ import React from 'react'
 import { StyleSheet, Text, View, StatusBar } from 'react-native'
 import { LinearGradient } from 'expo'
 import { primaryGradientArray } from './utils/Colors'
-import Header from './components/Header';
-const headerTitle = 'To Do List';
+import Header from './components/Header'
+import Input from './components/Input'
+const headerTitle = 'To Do List'
 
 export default class Main extends React.Component {
+  state = {
+    inputValue: ''
+  }
+  newInputValue = value => {
+    this.setState({
+      inputValue: value
+    })
+  }
+
   render() {
+    const { inputValue } = this.state
     return (
       <LinearGradient colors={primaryGradientArray} style={styles.container}>
         <View style={styles.centered}>
           <Header title={headerTitle} />
+        </View>
+        <View style={styles.inputContainer}>
+          <Input inputValue={inputValue} onChangeText={this.newInputValue} />
         </View>
       </LinearGradient>
     )
